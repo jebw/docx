c73c23d79b5c5c4bdf2fd86e3bb3c97ab0c09f6c
jebw <jeb@jdwilkins.co.uk>
2013-08-15 17:35:26 +0100

    Replaced delegate method with older Ruby1.8 version

--- lib/docx/core_ext/module.rb
+++ lib/docx/core_ext/module.rb
@@ -114,59 +114,40 @@ unless Object.const_defined?("ActiveSupport")
     #
     #   Foo.new.zoo   # returns nil
     def delegate(*methods)
-      options = methods.pop
-      unless options.is_a?(Hash) && to = options[:to]
-        raise ArgumentError, 'Delegation needs a target. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, to: :greeter).'
-      end
+	    options = methods.pop
+	    unless options.is_a?(Hash) && to = options[:to]
+	      raise ArgumentError, "Delegation needs a target. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, :to => :greeter)."
+	    end
 
-      prefix, allow_nil = options.values_at(:prefix, :allow_nil)
+	    if options[:prefix] == true && options[:to].to_s =~ /^[^a-z_]/
+	      raise ArgumentError, "Can only automatically set the delegation prefix when delegating to a method."
+	    end
 
-      if prefix == true && to =~ /^[^a-z_]/
-        raise ArgumentError, 'Can only automatically set the delegation prefix when delegating to a method.'
-      end
+	    prefix = options[:prefix] && "#{options[:prefix] == true ? to : options[:prefix]}_" || ''
 
-      method_prefix = \
-        if prefix
-          "#{prefix == true ? to : prefix}_"
-        else
-          ''
-        end
+	    file, line = caller.first.split(':', 2)
+	    line = line.to_i
 
-      file, line = caller.first.split(':', 2)
-      line = line.to_i
+	    methods.each do |method|
+	      on_nil =
+	        if options[:allow_nil]
+	          'return'
+	        else
+	          %(raise "#{self}##{prefix}#{method} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}")
+	        end
 
-      to = to.to_s
-      to = 'self.class' if to == 'class'
-
-      methods.each do |method|
-        # Attribute writer methods only accept one argument. Makes sure []=
-        # methods still accept two arguments.
-        definition = (method =~ /[^\]]=$/) ? 'arg' : '*args, &block'
-
-        if allow_nil
-          module_eval(<<-EOS, file, line - 2)
-            def #{method_prefix}#{method}(#{definition})        # def customer_name(*args, &block)
-              if #{to} || #{to}.respond_to?(:#{method})         #   if client || client.respond_to?(:name)
-                #{to}.#{method}(#{definition})                  #     client.name(*args, &block)
-              end                                               #   end
-            end                                                 # end
-          EOS
-        else
-          exception = %(raise "#{self}##{method_prefix}#{method} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}")
-
-          module_eval(<<-EOS, file, line - 1)
-            def #{method_prefix}#{method}(#{definition})        # def customer_name(*args, &block)
-              #{to}.#{method}(#{definition})                    #   client.name(*args, &block)
-            rescue NoMethodError                                # rescue NoMethodError
-              if #{to}.nil?                                     #   if client.nil?
-                #{exception}                                    #     # add helpful message to the exception
-              else                                              #   else
-                raise                                           #     raise
-              end                                               #   end
-            end                                                 # end
-          EOS
-        end
-      end
-    end
+	      module_eval(<<-EOS, file, line - 1)
+	        def #{prefix}#{method}(*args, &block)               # def customer_name(*args, &block)
+	          #{to}.__send__(#{method.inspect}, *args, &block)  #   client.__send__(:name, *args, &block)
+	        rescue NoMethodError                                # rescue NoMethodError
+	          if #{to}.nil?                                     #   if client.nil?
+	            #{on_nil}                                       #     return # depends on :allow_nil
+	          else                                              #   else
+	            raise                                           #     raise
+	          end                                               #   end
+	        end                                                 # end
+	      EOS
+	    end
+	  end
   end
 end
